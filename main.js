4f0b9797d22b97878f5c1aae451ff4f3be6a5d16
NyxDragon03 <kduque107@gmail.com>
2024-12-03 12:26:26 -0500

    corrección y mejora (main)

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
 //Logica frontend: controladores angular JS, llamar al API
 
-angular.module('angularToDo', []);
-
-function mainController($scope, $http){
+angular.module('angularToDo', [])
+    .controller('mainController',['$scope', '$http', function($scope, $http) {
     
-    $scope.formData = {};//almacenar variables dentro del controlador
+        $scope.formData = {};//almacenar variables dentro del controlador
 
-    //carga la pag y pide todos los ToDos del API
-    $http.get('/api/todos') //llamadas AJAX a la API con poco codigo
-        .success(function(data){
-        $scope.todos = data;
-        console.log(data)
-    })
-    .error(function(data){
-        console.log('Error: '+ data);
-        
-        
-    });
-    
-    //al añadir nuevo, envia el texto a la API
-    $scope.createToDo = function(){
-        $http.post('/api/todos', $scope.formData)
-        .success(function(data){
-            $scope.formData ={};
-            $scope.todos = data;
-            console.log('Error: '+ data);
-        });
-    };
-
-    //borra ToDo luego de marcarlo como terminado
-    $scope.deleteToDo = function(id){
-        $http.delete('/api/todos/'+ id)
-        .success(function(data){
-            $scope.todos = data;
-            console.log(data);
+        //carga la página y pide todos los ToDos del API
+        $http.get('/api/todos')
+            .then(function(response){
+            $scope.todos = response.data;
+            console.log(response.data)
         })
-        .error(function(data){
-            console.log('Error: '+ data);
+        .catch(function(error){
+            console.log('Error: '+ error.data);
         });
-    };
-}
+        
+        //añadir un nuevo ToDo, envía el texto a la API
+        $scope.createToDo = function(){
+            if($scope.formData.text){ //validar que el campo no esté vacío
+                $http.post('/api/todos', $scope.formrData)
+                .then(function(response){
+                    $scope.formrData = {};
+                    $scope.todos = response.data;
+                    console.log(response.data);
+                })
+                .catch(function(error){
+                    console.log('Error: '+ error,data)
+                });
+            } else {
+                alert('Error: El campo de texto está vacío');
+            }
+        };
+
+        //borra ToDo luego de marcarlo como terminado
+        $scope.deleteToDo = function(id){
+            $http.delete('/api/todos/'+ id)
+            .then(function(response){
+                $scope.todos = response.data;
+                console.log(response.data);
+            })
+            .catch(function(error){
+                console.log('Error: '+ error.data);
+            });
+        };
+    }]);
